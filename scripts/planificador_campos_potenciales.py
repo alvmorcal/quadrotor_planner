1367c3575f28070b88bb1c0e80168840e0ef4f0c
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2025-02-13 00:43:56 +0100

    Update planificador_campos_potenciales.py

--- scripts/planificador_campos_potenciales.py
+++ scripts/planificador_campos_potenciales.py
@@ -105,7 +105,7 @@ class DroneNavigator:
 
         # Parámetros para la detección de mínimos locales
         epsilon_force = 0.1       # umbral de fuerza para considerar que hay un mínimo local
-        local_min_threshold = 25   # número de iteraciones consecutivas con fuerza muy pequeña
+        local_min_threshold = 10   # número de iteraciones consecutivas con fuerza muy pequeña
         local_min_counter = 0
 
         trajectory = []  # almacena la trayectoria seguida
